f5d8cb798f286b5a91e9a8f3a7b60da3d575d6c9
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-08-03 16:35:49 -0400

    done

--- classes/person.js
+++ classes/person.js
@@ -9,17 +9,20 @@ class Person {
       console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
   }
   
-  static introducePeople (...People) {
-      if (!(Array.isArray(...People))) {
-          console.log(`introducePeople only takes an array as an argument.`)
+  static introducePeople (people) {
+      if (!(Array.isArray(people))) {
+          throw new Error(`introducePeople only takes an array as an argument.`)
       }
 
-      for (let person in People) {
-          if(person instanceof Person === false) {
-              console.log(`All items in array must be Person class instances`)
+      for (let i = 0; i < people.length; i++) {
+        let person = people[i];
+        
+        if(!(person instanceof Person)) {
+              throw new Error(`All items in array must be Person class instances.`)
           }
       }
-      
+
+      return people.forEach((person) => person.introduce())
   }
 }
 
@@ -31,6 +34,19 @@ class Person {
 
 // Jane.introduce()
 
+// const James = new Person(
+//   `James`,
+//   `Thompson`,
+//   `15`,
+// )
+
+// James.introduce()
+
+// let arr = [James, Jane]
+
+// let tester = Person.introducePeople(arr);
+
+// return tester
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
